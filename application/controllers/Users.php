02b3bedf37de7c12464ff5afdb1228fe80febff2
andsr157 <andikasatrio159@gmail.com>
2022-11-07 11:25:00 +0700

    membuat form validation untuk add new user

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -13,6 +13,30 @@ class Users extends CI_Controller{
     }
 
     public function add(){
-        $this->template->load('template', 'users/user_new');
+        $this->load->library('form_validation');
+
+        $this->form_validation->set_rules('nama','Nama','required');
+        $this->form_validation->set_rules('username','Username','required|min_length[5]|is_unique[users.username]');
+        $this->form_validation->set_rules('password','Password','required|required|min_length[5]');
+        $this->form_validation->set_rules('passconf','Konfirmasi Password','required|matches[password]',
+            array('matches' => '%s tidak sesuai dengan password'));
+
+        // $this->form_validation->set_rules('email','Email','required|email_valid',
+        // array('email_valid' => 'masukan email yang valid'));
+        
+        $this->form_validation->set_rules('email','Email','required');
+        $this->form_validation->set_rules('level','Posisi','required');
+
+        $this->form_validation->set_message('required', '%s harus di isi!!');
+        $this->form_validation->set_message('min_length', '%s minimal 5 karakter');
+        
+
+        $this->form_validation->set_error_delimiters('<span class="text-danger" style="font-size:12px;">','</span>');
+
+        if ($this->form_validation->run() == FALSE){
+            $this->template->load('template', 'users/user_form_add');
+        }else{
+            echo"SUCCESS";
+        }
     }
-}
+}   
